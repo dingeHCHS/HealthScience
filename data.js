235ce8a92e68ff7b3c130dabf94b54b2b79c3126
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 10:54:27 -0600

    SUM

---
+++ data.js
@@ -0,0 +1,145 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-healthscience",
+      "name": "HealthScience",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.006432458085184223,
+          "pitch": -0.03927769436709383,
+          "title": "HS1",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.29888441578735403,
+          "pitch": -0.03617101458569216,
+          "title": "HS2",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5878845405371855,
+          "pitch": -0.03917495461885423,
+          "title": "HS3",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.8751314031354376,
+          "pitch": -0.023992187880912397,
+          "title": "HS4",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3461230506735458,
+          "pitch": -0.03093301332420495,
+          "title": "HS5",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.651261277799815,
+          "pitch": -0.02576094551838537,
+          "title": "HS6",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0028229333384306,
+          "pitch": -0.033813083269992106,
+          "title": "HS7",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3679815786573766,
+          "pitch": -0.019147793609718633,
+          "title": "HS8",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.659443864909745,
+          "pitch": -0.025153568235690926,
+          "title": "HS9",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.9404302341728634,
+          "pitch": -0.027917908891538445,
+          "title": "HS10",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.882978023514397,
+          "pitch": -0.01757458223592323,
+          "title": "HS11",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.5321501334763745,
+          "pitch": 0.00036850202269178567,
+          "title": "HS12",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2797547068564086,
+          "pitch": 0.0018400086791601211,
+          "title": "HS13",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8471281779740476,
+          "pitch": 0.01792048919586975,
+          "title": "HS14",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4075412864874064,
+          "pitch": 0.03393110719957093,
+          "title": "HS15",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.0483891537772116,
+          "pitch": 0.03561892856940574,
+          "title": "HS16",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5700671200013527,
+          "pitch": 0.032044387805513,
+          "title": "HS17",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
