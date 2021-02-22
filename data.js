c4ec4113c0ebd978aca4d01efb3a35b8f6fa899d
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:03:05 -0600

    Update data.js

--- data.js
+++ data.js
@@ -34,103 +34,97 @@ var APP_DATA = {
           "yaw": -0.006432458085184223,
           "pitch": -0.03927769436709383,
           "title": "HS1",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1IIcpYmLuOaXiH6-KcvFYnv8HAsjfT9hm/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.29888441578735403,
           "pitch": -0.03617101458569216,
           "title": "HS2",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1IPUgOtsqCgsL4qifLc-mTN4t-lcZorMG/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.5878845405371855,
           "pitch": -0.03917495461885423,
           "title": "HS3",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1J8CAEahjGrCvOkBP7lRPSr6M0ZkWLeNT/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.8751314031354376,
           "pitch": -0.023992187880912397,
           "title": "HS4",
-          "text": "Text"
+          "text": ''
         },
         {
           "yaw": 1.3461230506735458,
           "pitch": -0.03093301332420495,
           "title": "HS5",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1J3VCNNMJc8Dr8pkFzh-KH1iZofJeoYyc/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 1.651261277799815,
           "pitch": -0.02576094551838537,
           "title": "HS6",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1J2ZH0t-hcwdP6x30GQb1M5YLuCOcFqHU/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.0028229333384306,
           "pitch": -0.033813083269992106,
           "title": "HS7",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1IW5pPXogsLc6PfixG8wf2d0vqu1l2kyh/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.3679815786573766,
           "pitch": -0.019147793609718633,
           "title": "HS8",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1J-zycnXclOLyHYapK10n2T9P78o_a0Kl/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.659443864909745,
           "pitch": -0.025153568235690926,
           "title": "HS9",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1J-6xc19TOwcR5jpHXHsg1rs_lSw6QJeN/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.9404302341728634,
           "pitch": -0.027917908891538445,
           "title": "HS10",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1Iy0U79mjTZJdo8onzedXEQUJMnxOaLeR/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -2.882978023514397,
           "pitch": -0.01757458223592323,
           "title": "HS11",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1IvL4m6Zb6MqSONAEvWJ_C-zmuJR0TRiI/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -2.5321501334763745,
           "pitch": 0.00036850202269178567,
           "title": "HS12",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1It-T5muBHhtHGUIU50U-xnMqxn2JQmeJ/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -2.2797547068564086,
           "pitch": 0.0018400086791601211,
           "title": "HS13",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1Ir02zkrkNQirsxYqOlArs8vzYpBLDCEU/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -1.8471281779740476,
           "pitch": 0.01792048919586975,
           "title": "HS14",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1Ik5jr6g8v6Y7urwoiCL9UICAn8kiONm1/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -1.4075412864874064,
           "pitch": 0.03393110719957093,
           "title": "HS15",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1IRBonFainfznEQ-K-km54VJTWWO82zXH/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -1.0483891537772116,
           "pitch": 0.03561892856940574,
           "title": "HS16",
-          "text": "Text"
-        },
-        {
-          "yaw": -0.5700671200013527,
-          "pitch": 0.032044387805513,
-          "title": "HS17",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1J25QqtGTJFtdv3SkyHrCh-HpKPSbQatA/preview" width="640" height="480"></iframe>'
         }
       ]
     }
